85a3aa7c27d5e279689894bb1be8120819cf1fe8
brandon <bresheske@gmail.com>
2020-03-22 14:26:49 -0500

    added launch

--- dbconf.js
+++ dbconf.js
@@ -1,11 +1,13 @@
 const args = require('yargs').argv;
 const glob = require('glob');
-const fs = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
+const { spawn } = require('child_process');
+const { dirname } = require('path');
 
 const root = 'D:\\SteamGames\\steamapps\\common\\King\'s Quest Collection';
 
-if (!args.fullscreen && !args.window) {
-    console.log(`Options requires 'fullscreen' or 'window'`);
+if (!args.fullscreen && !args.window && !args.launch) {
+    console.log(`options requires 'fullscreen' or 'window'`);
     return;
 }
 else if (args.window && !args.size) {
@@ -13,16 +15,44 @@ else if (args.window && !args.size) {
     return;
 }
 
-const files = glob.sync(`${root}/**/dosbox.conf`);
-for(const file of files) {
-    let data = fs.readFileSync(file).toString();
-    
-    if (args.fullscreen) {
-        data = data.replace(/fullscreen=.*/g, 'fullscreen= true');
+const configAction = () => {
+    const files = glob.sync(`${root}/**/dosbox.conf`);
+    for(const file of files) {
+        let data = readFileSync(file).toString();
+        
+        if (args.fullscreen) {
+            data = data.replace(/fullscreen=.*/g, 'fullscreen= true');
+        }
+        else if (args.window) {
+            data = data.replace(/fullscreen=.*/g, 'fullscreen= false');
+            data = data.replace(/windowresolution=.*/g, `windowresolution= ${args.size}`);
+        }
+        writeFileSync(file, data);
     }
-    else if (args.window) {
-        data = data.replace(/fullscreen=.*/g, 'fullscreen= false');
-        data = data.replace(/windowresolution=.*/g, `windowresolution= ${args.size}`);
+};
+
+const launchAction = () => {
+    const exes = glob.sync(`${root}/**/*${args.launch}*.exe`);
+    if (exes && exes[0]) {
+        const cmd = exes[0];
+        console.log(`launching '${cmd}'`);
+        spawn(cmd, 
+            { 
+                cwd: dirname(exes[0]),
+                detached: true,
+                stdio: 'ignore'
+            }).unref();
     }
-    fs.writeFileSync(file, data);
+    else {
+        console.log(`could not launch ${args.launch}. no executable found.`);
+    }
+};
+
+// main entrypoint of the app.
+
+if (args.fullscreen || args.window) {
+    configAction();
+}
+else if (args.launch) {
+    launchAction();
 }
--- readme.md
+++ readme.md
@@ -10,12 +10,14 @@ Examples:
 
  - set fullscreen: `node dbconf --fullscreen`
  - set window: `node dbconf --window --size 1200x900`
+ - launch a game (a game with '5' in the name): `node dbconf --launch 5`
 
 ## Options
 
  - `--fullscreen`: boolean: sets all files to fullscreen mode.
  - `--window`: boolean: sets all files to window mode.
  - `--size`: string: sets dosbox's 'windowresolution' setting.
+ - `--launch`: string: launches a game with 'string' in the name.
 
 ## Root
 
